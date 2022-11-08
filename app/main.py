6355d0166fd1679e44fa822145f39039035d07ef
juanfu26 <juant.cas@gmail.com>
2022-11-09 00:51:01 +0100

    Add root_path and basic webhook endpoint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(root_path="/alfredo")
 
 @app.get("/")
 def root():
-    return {"message": "FastAPI in Python"}
+    return {"message": "Hello world"}
+
+@app.post('/webhook')
+def webhook():
+  return {
+        "fulfillmentText": 'This is from the replit webhook',
+        "source": 'webhook'
+    }
